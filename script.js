916c11ab2c85d113b104eb1886be509179c74720
michaelyurachek <michaelyurachek@gmail.com>
2025-01-25 23:13:48 -0500

    UPDATE

---
+++ script.js
@@ -0,0 +1,11 @@
+var username = 'babelockin_0101';
+var password = 'ireallylikelegos';
+
+function loginCheck() {
+    const inputUser = document.getElementById("username").value;
+    const inputPass = document.getElementById("password").value;
+    if (inputUser == username && inputPass == password) {
+        document.getElementById("loginSection").style.display = "none";
+        document.getElementById("showSection").style.display = "flex";
+    }
+}
